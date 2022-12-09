4fa563fa4bce86a065c17a950e6dfcb41a37f35b
ZLourenso25 <josesalviano75@gmail.com>
2022-12-09 09:22:49 -0300

    commit 1

---
+++ meuPrimeiroPrograma.java
@@ -0,0 +1,24 @@
+//salvar como MeuPrimeiroProgramaJava.java
+
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+  //Modulo principal com a entrada pela linha de comando 
+  public static void main(String entrada[])
+  {
+	//declaração de variáveis
+	  int inteiro = 20;
+	  char caracter = 'F';
+	  double real = 1.77;
+	  String frase = " José Lourenço ";
+	  boolean VF = true;
+
+	  if (VF == true)
+	  {
+	    System.out.println("Eu sou o " + frase + "tenho " + inteiro + " anos e tenho " + real + "m de altura");
+	  }
+
+		System.exit(0);
+ }
+}
+		
